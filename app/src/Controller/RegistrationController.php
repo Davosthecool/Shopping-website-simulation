a71698d059f8d661f5496669b87a18ebc6eda033
David CHOCHO <E212269C@ubuntu.u-inf-j-c114-21>
2024-01-09 15:02:30 +0100

    ajout pages connexions en twig et controlleurs associés

--- app/src/Controller/RegistrationController.php
+++ app/src/Controller/RegistrationController.php
@@ -36,7 +36,7 @@ class RegistrationController extends AbstractController
             return $this->redirectToRoute('app_test');
         }
 
-        return $this->render('registration/register.html.twig', [
+        return $this->render('connexion/register.html.twig', [
             'registrationForm' => $form->createView(),
         ]);
     }
