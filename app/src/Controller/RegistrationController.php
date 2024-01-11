89b5603295e70311ec580ebd8f64b60d90ceb07c
ibrahim <ibrahim@Ubuntu.myguest.virtualbox.org>
2024-01-11 17:16:31 +0100

    Amélioration de l'authentification

--- app/src/Controller/RegistrationController.php
+++ app/src/Controller/RegistrationController.php
@@ -4,25 +4,23 @@ namespace App\Controller;
 
 use App\Entity\User;
 use App\Form\RegistrationFormType;
+use App\Security\UsersAuthenticator;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Http\Authentication\UserAuthenticatorInterface;
 
 class RegistrationController extends AbstractController
 {
     #[Route('/register', name: 'app_register')]
-    public function register(Request $request, UserPasswordHasherInterface $userPasswordHasher, EntityManagerInterface $entityManager): Response
+    public function register(Request $request, UserPasswordHasherInterface $userPasswordHasher, EntityManagerInterface $entityManager,
+                             UserAuthenticatorInterface $userAuthenticator, UsersAuthenticator $authenticator): Response
     {
-        $session = $request->getSession();
-
-        if (!$session->has("email") || $session->get("email") == null)
-            $this->redirectToRoute("app_authenticate");
-        if ($session->get("permission"))
-            $this->redirectToRoute("app_test"); //à modifier
-
+        if ($this->getUser())
+            return $this->redirectToRoute('app_test');
         $user = new User();
         $form = $this->createForm(RegistrationFormType::class, $user);
         $form->handleRequest($request);
@@ -36,22 +34,23 @@ class RegistrationController extends AbstractController
                     'registrationForm' => $form->createView(),
                 ]);
             }
-            $user->setEmail($session->get("email"));
             $user->setRoles(array("user"));
-            // encode the plain password
             $user->setPassword(
                 $userPasswordHasher->hashPassword(
                     $user,
                     $form->get('password')->getData()
                 )
             );
-            $this->verifierChamps($user);
 
             $entityManager->persist($user);
             $entityManager->flush();
             // do anything else you need here, like send an email
 
-            return $this->redirectToRoute('app_produit');
+            return $userAuthenticator->authenticateUser(
+                $user,
+                $authenticator,
+                $request
+            );
         }
         return $this->render('connexion/register.html.twig', [
             'registrationForm' => $form->createView(),
--- app/src/Form/RegistrationFormType.php
+++ app/src/Form/RegistrationFormType.php
@@ -5,13 +5,16 @@ namespace App\Form;
 use App\Entity\User;
 use Doctrine\DBAL\Types\StringType;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\ButtonType;
 use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
 use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\Email;
 use Symfony\Component\Validator\Constraints\EqualTo;
 use Symfony\Component\Validator\Constraints\IsTrue;
 use Symfony\Component\Validator\Constraints\Length;
@@ -22,6 +25,17 @@ class RegistrationFormType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
+            ->add('email', EmailType::class,[
+                'attr' => ['autocomplete' => false],
+                'constraints' => [
+                    new NotBlank([
+                        'message' => 'Veuillez saisir votre email',
+                    ]),
+                    new Email([
+                        'message' => 'E-mail incorrect'
+                    ])
+                ]
+            ])
             ->add('nom', null,[
                 'constraints' => [
                     new NotBlank([
@@ -45,7 +59,7 @@ class RegistrationFormType extends AbstractType
                 'expanded' => true,
             ])
             ->add('password', RepeatedType::class, [
-                               
+
                 'type' => PasswordType::class,
                 'first_options' => ['label' => 'Mot de passe'],
                 'second_options' => ['label' => 'Confirmer le mot de passe'],
@@ -63,7 +77,11 @@ class RegistrationFormType extends AbstractType
                     ]),
                 ],
             ])
-            ->add('submit', SubmitType::class)
+            ->add('submit', SubmitType::class, [
+                'attr' => [
+                    'label' => "S'inscrire"
+                ]
+            ])
         ;
     }
 
