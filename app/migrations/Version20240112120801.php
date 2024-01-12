a12b38a9cb8ac229209d52d5ea73d1f7c7b80f5f
Bastian MARY <E225407H@ubuntu.u-inf-j-c114-26>
2024-01-12 13:15:22 +0100

    header_accueil_css

--- app/migrations/Version20240112120801.php
+++ app/migrations/Version20240112120801.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20240112072532 extends AbstractMigration
+final class Version20240112120801 extends AbstractMigration
 {
     public function getDescription(): string
     {
