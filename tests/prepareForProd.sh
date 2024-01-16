ffd1ee2d54400ea0da835e0406f2274af982930a
David CHOCHO <david.chocho@etu.univ-nantes.fr>
2024-01-16 12:03:13 +0000

    Update prepareForProd.sh

--- tests/prepareForProd.sh
+++ tests/prepareForProd.sh
@@ -3,7 +3,7 @@ git pull
 cd  ..
 sudo chmod 777 -R Flow/
 cd Flow/
-./test/init.sh
+./tests/init.sh
 php composer.phar dump-env prod
 php composer.phar dump-autoload -o --no-dev
 echo "[OK] Ready for prod"
